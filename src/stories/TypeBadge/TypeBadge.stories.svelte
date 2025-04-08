6d4302fc87dc1a8c855b41159e4b1bb74b34a37d
Artofaj <info@artofakume.com>
2025-04-08 17:27:44 +0200

    feat: Add basic TypeBadge

---
+++ src/stories/TypeBadge/TypeBadge.stories.svelte
@@ -0,0 +1,92 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import TypeBadge from "./TypeBadge.svelte";
+
+  const { Story } = defineMeta({
+    title: "TypeBadge",
+    component: TypeBadge,
+    argTypes: {
+      type: { name: "text" },
+    },
+  });
+</script>
+
+<Story name="Normal">
+  <TypeBadge type="normal" />
+</Story>
+
+<Story name="Fire">
+  <TypeBadge type="fire" />
+</Story>
+
+<Story name="Water">
+  <TypeBadge type="water" />
+</Story>
+
+<Story name="Electric">
+  <TypeBadge type="electric" />
+</Story>
+
+<Story name="Grass">
+  <TypeBadge type="grass" />
+</Story>
+
+<Story name="Ice">
+  <TypeBadge type="ice" />
+</Story>
+
+<Story name="Fighting">
+  <TypeBadge type="fighting" />
+</Story>
+
+<Story name="Poison">
+  <TypeBadge type="poison" />
+</Story>
+
+<Story name="Ground">
+  <TypeBadge type="ground" />
+</Story>
+
+<Story name="Flying">
+  <TypeBadge type="flying" />
+</Story>
+
+<Story name="Psychic">
+  <TypeBadge type="psychic" />
+</Story>
+
+<Story name="Bug">
+  <TypeBadge type="bug" />
+</Story>
+
+<Story name="Rock">
+  <TypeBadge type="rock" />
+</Story>
+
+<Story name="Ghost">
+  <TypeBadge type="ghost" />
+</Story>
+
+<Story name="Steel">
+  <TypeBadge type="steel" />
+</Story>
+
+<Story name="Dragon">
+  <TypeBadge type="dragon" />
+</Story>
+
+<Story name="Dark">
+  <TypeBadge type="dark" />
+</Story>
+
+<Story name="Fairy">
+  <TypeBadge type="fairy" />
+</Story>
+
+<Story name="Shadow">
+  <TypeBadge type="shadow" />
+</Story>
+
+<Story name="Stellar">
+  <TypeBadge type="stellar" />
+</Story>
---
+++ src/stories/TypeBadge/TypeBadge.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { typeColors } from "./colors";
+
+  export let type: string;
+</script>
+
+<div
+  class="type-badge"
+  title={type}
+  style={`--pokemon-type-color: ${typeColors[type as keyof typeof typeColors] || typeColors.normal};`}
+></div>
+
+<style>
+  .type-badge {
+    aspect-ratio: 1 / 1;
+    border-radius: 100%;
+    min-width: 0.5rem;
+    max-width: 2rem;
+    background-color: var(--pokemon-type-color);
+  }
+</style>
