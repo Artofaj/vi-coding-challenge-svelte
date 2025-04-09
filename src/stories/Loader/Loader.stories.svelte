46bffb217d2ddbe5d4083488fc14aa076d7e34fe
Artofaj <info@artofakume.com>
2025-04-09 15:41:16 +0200

    feat: Add a loader component

---
+++ src/stories/Loader/Loader.stories.svelte
@@ -0,0 +1,16 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import Loader from "./Loader.svelte";
+
+  const { Story } = defineMeta({
+    title: "Loader",
+    component: Loader,
+  });
+</script>
+
+<Story name="Default">
+  <Loader />
+</Story>
+<Story name="With text">
+  <Loader>Loading</Loader>
+</Story>
