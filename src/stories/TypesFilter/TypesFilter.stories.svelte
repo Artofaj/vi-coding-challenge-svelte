3b8968436545cdffb38f64bab0aa584b68e63f07
Artofaj <info@artofakume.com>
2025-04-10 11:26:33 +0200

    feat: Refine story of TypeFilter
    This adds a select and a dropdown

--- src/stories/TypesFilter/TypesFilter.stories.svelte
+++ src/stories/TypesFilter/TypesFilter.stories.svelte
@@ -1,13 +1,19 @@
 <script module>
   import { defineMeta } from "@storybook/addon-svelte-csf";
   import TypesFilter from "./TypesFilter.svelte";
+  import { allPokemonTypes } from "../TypeBadge/colors";
 
   const { Story } = defineMeta({
     title: "TypesFilter",
     component: TypesFilter,
+    tags: ["autodocs"],
+    argTypes: {
+      selectedType: {
+        control: { type: "select" },
+        options: [...Object.values(allPokemonTypes), null],
+      },
+    },
   });
 </script>
 
-<Story name="Empty">
-  <TypesFilter />
-</Story>
+<Story name="Empty" argTypes={{ selectedType: { defaultValue: null } }} />
